7eda4d02830856ea1695db941f34a16191d3f975
XuanTruong <hdnk.it@gmail.com>
2023-06-23 10:53:09 +0700

    main src
    About extension's source

---
+++ GIdailychecker/background.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+chrome.runtime.onStartup.addListener(function () {
+    setTimeout(onload, 5000);
+});
+async function onload() {
+    try {
+        let res = await fetch(
+            "https://sg-hk4e-api.hoyolab.com/event/sol/sign?",
+            { mode: 'no-cors',
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify({ act_id: "e202102251931481" }),
+              credentials: "include"
+            });
+        let text = await res.text();
+        console.log(text);
+    }
+    catch (e) {
+        console.log(e);
+        setTimeout(onload, 5000);
+    }
+}
